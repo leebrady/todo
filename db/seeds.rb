3eb5dc13f592ddbb9a50a5525c5f1e48602d4dc8
Lee Brady <leebrady0114@gmail.com>
2022-04-11 23:14:18 +0100

    'addeddbseedmethod'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+	task_list = [
+		["Completed Task", true],
+		["Uncompleted Task", true]
+	]
+
+task_list.each do |description, completed|
+	List.create( description: description, completed: completed )
+end
+
+
+
+	# create([{description: 'Test Task', completed: true}])
